6f9c12f2958f09fc1e9bcddd69cdac9c6fa768a7
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-23 17:23:59 +0300

    use: empity tag for JSX wraper

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -3,10 +3,10 @@ import { Component } from 'react'
 export default class TodoContainer extends Component {
   render() {
     return (
-      <div>
+      <>
         <h1>Hello Create React App</h1>
         <p>I am in a React Component!</p>
-      </div>
+      </>
     )
   }
 }
