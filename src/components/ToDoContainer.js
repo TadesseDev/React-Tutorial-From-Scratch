bca02861a40b839f87312dbfc9586377335a494f
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-23 17:14:30 +0300

    Create: and render the first classfull component

---
+++ src/components/ToDoContainer.js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+
+export default class TodoContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Create React App</h1>
+        <p>I am in a React Component!</p>
+      </div>
+    )
+  }
+}
