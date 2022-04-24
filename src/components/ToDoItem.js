60ca21384ff19ac47f5b10ad647efdbf3097b764
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-24 18:02:53 +0300

    Use distructuring and complet styling

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-
+import styles from "./ToDoItem.module.css"
 class ToDoItem extends Component {
   render() {
+    const { completed, id, title } = this.props.task;
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type="checkbox"
-          checked={this.props.task.completed}
-          onChange={() => this.props.changeHandlerProp(this.props.task.id)}
+          checked={completed}
+          onChange={() => this.props.changeHandlerProp(id)}
         />
-        <button onClick={() => this.props.deleteHandler(this.props.task.id)}>Delete</button>
-        {this.props.task.title}
+        <button onClick={() => this.props.deleteHandler(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
       </li>
     );
   }
