673e3434ff7e8f3f91cbf7ec31d6fd93946da5b8
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-24 17:17:51 +0300

    Add App.css file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import TodoContainer from './components/ToDoContainer'
+import './App.css'
 ReactDOM.render(<React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById('root'));
