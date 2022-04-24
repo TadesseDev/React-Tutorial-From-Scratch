fbb33271b02c2c1ebfd51a50c3576efdebdbb6ca
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-24 17:29:47 +0300

    Add style to the inputToDo component

--- src/components/InputToDo.js
+++ src/components/InputToDo.js
@@ -19,9 +19,15 @@ export default class InputToDo extends Component {
   }
   render() {
     return (
-      <form action="#" method="POST" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Add ToDo..." value={this.state.title} onChange={this.updateTaskTitle} />
-        <button type="submit">Submit</button>
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          placeholder="Add ToDo..."
+          value={this.state.title} onChange={this.updateTaskTitle}
+          className="input-text" />
+        <button
+          type="submit"
+          className="input-submit">Submit</button>
       </form>
     )
   }
