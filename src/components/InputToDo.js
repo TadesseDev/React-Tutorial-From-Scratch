204d10b6fefda693adf5ebfd04e32b3f048ebb51
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-24 15:46:48 +0300

    make the tinput field contolled element

--- src/components/InputToDo.js
+++ src/components/InputToDo.js
@@ -1,9 +1,18 @@
 import { Component } from "react";
 export default class InputToDo extends Component {
+  state = {
+    title: "",
+  }
+  updateTaskTitle = (e) => {
+    this.setState((oldState) => ({
+      ...oldState,
+      title: e.target.value
+    }));
+  }
   render() {
     return (
       <form action="#" method="post">
-        <input type="text" placeholder="Add ToDo..." />
+        <input type="text" placeholder="Add ToDo..." value={this.state.title} onChange={this.updateTaskTitle} />
         <button type="submit">Submit</button>
       </form>
     )
