acff4dbc31d38dce4ba371dd14aea2491bde33f0
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-25 18:38:51 +0300

    Add routes for About and NotFoud components

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import TodoContainer from './functionBased/components/ToDoContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './functionBased/pages/About'
+import NotMatch from './functionBased/pages/NotMatch'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
