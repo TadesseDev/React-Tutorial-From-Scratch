a176edc125fe5e8f670c764a2097de16aef1182e
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-25 13:38:19 +0300

    Add useEffect hook in TodoContainer

--- src/functionBased/components/ToDoContainer.js
+++ src/functionBased/components/ToDoContainer.js
@@ -8,57 +8,63 @@ const TodoContainer = (props) => {
 
   function getInitialTodos() {
     return JSON.parse(localStorage.getItem('todos')) || [];
+  }
+  const [todos, setTodos] = useState(getInitialTodos);
 
-    const handleChange = (id) => {
-      this.setTodos((oldState) => (
-        oldState.todos.map(todo => {
-          if (todo.id === id)
-            todo.completed = !todo.completed;
-          return todo
-        }
-        )
-      )
-      );
-    }
-    const delTask = id => {
-      setTodos(oldState => (
-        oldState.filter(todo => todo.id !== id)
-      ))
-      console.log('deleting task ' + id);
-    }
-    const addTask = (title) => {
-      setTodos(oldState => ([
-        ...oldState,
-        {
-          id: uuid(),
-          title: title,
-          completed: false
-        }]
-      ))
-    }
-    const setUpdate = (updatedTitle, id) => {
-      setTodos(todos.map(todo => {
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+    console.log('updating todo')
+  }, [todos]);
+  const handleChange = (id) => {
+    this.setTodos((oldState) => (
+      oldState.todos.map(todo => {
         if (todo.id === id)
-          todo.title = updatedTitle;
-        return todo;
-      })
-      );
-    }
-
-    return (
-      <div id="container">
-        <div className="inner">
-          <Header />
-          <InputToDo addTask={addTask} />
-          <ToDoList
-            toDoList={todos}
-            changeHandlerProp={handleChange}
-            deleteHandler={delTask}
-            setUpdate={setUpdate}
-          />
-        </div>
-      </div>
+          todo.completed = !todo.completed;
+        return todo
+      }
+      )
     )
+    );
+  }
+  const delTask = id => {
+    setTodos(oldState => (
+      oldState.filter(todo => todo.id !== id)
+    ))
+    console.log('deleting task ' + id);
+  }
+  const addTask = (title) => {
+    setTodos(oldState => ([
+      ...oldState,
+      {
+        id: uuid(),
+        title: title,
+        completed: false
+      }]
+    ))
+  }
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(todos.map(todo => {
+      if (todo.id === id)
+        todo.title = updatedTitle;
+      return todo;
+    })
+    );
   }
 
-  export default TodoContainer;
+  return (
+    <div id="container">
+      <div className="inner">
+        <Header />
+        <InputToDo addTask={addTask} />
+        <ToDoList
+          toDoList={todos}
+          changeHandlerProp={handleChange}
+          deleteHandler={delTask}
+          setUpdate={setUpdate}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default TodoContainer;
