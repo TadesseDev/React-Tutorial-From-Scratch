4f565b7cdd68bb56859fd29680b608fc8d89c1c2
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-25 21:13:32 +0300

    Add Nested and dynamic Routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import TodoContainer from './functionBased/components/ToDoContainer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Navbar from './functionBased/components/Navbar'
 ReactDOM.render(
   <React.StrictMode>
